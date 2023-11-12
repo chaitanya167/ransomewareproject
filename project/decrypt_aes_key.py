5ede7a304cdf399be52ffdad8bf672c5a72e4ec7
chaitanya167 <61372406+chaitanya167@users.noreply.github.com>
2023-11-12 13:58:17 -0600

    Update public and private key decrypt_aes_key.py

--- project/decrypt_aes_key.py
+++ project/decrypt_aes_key.py
@@ -4,10 +4,29 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import hashes  # Ensure 'hashes' is correctly imported
-from getpass import getpass
+from cryptography.hazmat.primitives import hashes  # Correct import
 
-def decrypt_folder(folder_path, aes_key):
+def decrypt_folder(folder_path, private_key):
+    with open('encrypted_aes_key.bin', 'rb') as key_file:
+        encrypted_aes_key = key_file.read()
+
+    # Debug: Print the loaded private key
+    print("Loaded Private Key:", private_key)
+
+    # Decrypt the AES key with the private key
+    try:
+        aes_key = private_key.decrypt(
+            encrypted_aes_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+    except Exception as e:
+        print("Decryption Error:", e)
+        raise
+
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
@@ -21,16 +40,21 @@ def decrypt_folder(folder_path, aes_key):
             with open(file_path, 'wb') as f:
                 f.write(decrypted_data)
 
+
 def main():
     # Define the path to the folder to be decrypted
     folder_to_decrypt = "/home/sec-lab/Dummy"
 
-    # Load the AES key
-    with open('aes_key.bin', 'rb') as key_file:
-        aes_key = key_file.read()
+    # Load the unencrypted private key
+    with open('private_key.pem', 'rb') as private_key_file:
+        private_key = serialization.load_pem_private_key(
+            private_key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
 
     # Decrypt each file in the folder
-    decrypt_folder(folder_to_decrypt, aes_key)
+    decrypt_folder(folder_to_decrypt, private_key)
 
     print("Folder decrypted and unlocked successfully.")
 
